5779b7d36ec3dc9829be796db545f4fd20837aed
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2022-12-09 19:07:38 +0100

    Update basket method

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,7 @@
+from decimal import Decimal
+from store.models import Product
+
+
 class Basket:
     """
     A base Basket class, providing some default behaviors that
@@ -11,12 +15,40 @@ class Basket:
             basket = self.session["skey"] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, qty):
         """
         Adding and updating the users basket session data
         """
         product_id = product.id
         if product_id not in self.basket:
-            self.basket[product_id] = {"price": str(product.price)}
+            self.basket[product_id] = {"price": str(product.price), "qty": int(qty)}
 
         self.session.modified = True
+
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        prodyct_ids = self.basket.keys()
+        products = Product.products.filter(id__in=prodyct_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]["product"] = product
+
+        for item in basket.values():
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["qty"]
+            yield item
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item["qty"] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum(
+            Decimal(item["price"]) * item["qty"] for item in self.basket.values()
+        )
--- basket/views.py
+++ basket/views.py
@@ -6,18 +6,18 @@ from store.models import Product
 
 
 def basket_summary(request):
-    return render(
-        request,
-        "store/basket/summary.html",
-    )
+    basket = Basket(request)
+    return render(request, "store/basket/summary.html", {"basket": basket})
 
 
 def basket_add(request):
-    print("elo", request)
     basket = Basket(request)
     if request.POST.get("action") == "post":
         product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product)
-        response = JsonResponse({"test": "data"})
+        basket.add(product=product, qty=product_qty)
+
+        basketqty = basket.__len__()
+        response = JsonResponse({"qty": basketqty})
         return response
